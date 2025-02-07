8b884545091bf95ae2e66c44bd8bf46517d65aa4
= <=>
2025-02-07 22:35:20 +0530

    Initial commit

---
+++ Basics/target/debug/deps/Basics-628ec37958fc732f.d
@@ -0,0 +1,5 @@
+/home/cybysoul/Desktop/Learning Rust/Basics/target/debug/deps/libBasics-628ec37958fc732f.rmeta: src/main.rs
+
+/home/cybysoul/Desktop/Learning Rust/Basics/target/debug/deps/Basics-628ec37958fc732f.d: src/main.rs
+
+src/main.rs:
---
+++ Basics/target/debug/deps/Basics-f69fb9a725d5011f.d
@@ -0,0 +1,5 @@
+/home/cybysoul/Desktop/Learning Rust/Basics/target/debug/deps/libBasics-f69fb9a725d5011f.rmeta: src/main.rs
+
+/home/cybysoul/Desktop/Learning Rust/Basics/target/debug/deps/Basics-f69fb9a725d5011f.d: src/main.rs
+
+src/main.rs:
